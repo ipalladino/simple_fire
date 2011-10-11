d03549886b9fd9911f6bfbeb8b6309e51bf6c728
mindless <ipalladino@gmail.com>
2011-10-10 17:55:20 -0700

    Added js files to handle AJAX requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,22 +22,19 @@ class PagesController < ApplicationController
     @title = "Help"
   end
   
-  def test
-    @title = "Home"
+  def getproducts    
     api = "1c407a7cf65eb1c79a9a1883830be87b"
     password = "90fc2c2816609950da97db0135806369"
-    
     http = Net::HTTP.new('leuschke-inc8683.myshopify.com')
     #http.use_ssl = true
     http.start do |http|
       req = Net::HTTP::Get.new('/admin/products.xml')
-
       # we make an HTTP basic auth by passing the
       # username and password
       req.basic_auth api, password
       resp, data = http.request(req)
-      @body = data
-    end
+      render :xml => data 
+    end    
   end
   
 end
