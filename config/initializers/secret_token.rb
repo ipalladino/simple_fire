ca3c8f2d6fd68041470c5c135b48eb746a97a4c5
mindless <ipalladino@gmail.com>
2011-10-10 17:10:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D2mECards::Application.config.secret_token = '4150edbbc329f4d00188df521812baea09eadf56bf3450568ed2915cbfb0db943935928eb87d32dd66e3723466ff6e0ac234b51291fcfb36b718a73ffb344bae'
