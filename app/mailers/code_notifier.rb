29ba318e01693f520f8bcbae739afc66ed055050
mindless <ipalladino@gmail.com>
2011-11-08 01:14:06 -0800

    Bug fixing email not working as expected, switched txt to html

--- app/mailers/code_notifier.rb
+++ app/mailers/code_notifier.rb
@@ -3,15 +3,15 @@ class CodeNotifier < ActionMailer::Base
           :return_path => 'system@example.com'
 
   def welcome(recipient)
-  @account = recipient
-  mail(:to => recipient[:email],
-       :bcc => ["bcc@example.com", "Order Watcher <watcher@example.com>"])
+    @account = recipient
+    mail(:to => recipient[:email],
+         :bcc => ["bcc@example.com", "Order Watcher <watcher@example.com>"])
   end
   
   def recipientecardnotification(content)
     @content = content
     mail(:to => content[:email],                                          
-       :bcc => [content[:senderemail], "Order Watcher <ipalladino@gmail.com>"])  
+         :bcc => [content[:senderemail], "Order Watcher <ipalladino@gmail.com>"])  
   end
 end
 
