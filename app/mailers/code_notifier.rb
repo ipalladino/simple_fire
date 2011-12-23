5b0a417f333ac8970e9be0aaf8eed41988289141
mindless <ipalladino@gmail.com>
2011-12-23 13:28:57 -0300

    Changed subject of welcome message

--- app/mailers/code_notifier.rb
+++ app/mailers/code_notifier.rb
@@ -6,7 +6,7 @@ class CodeNotifier < ActionMailer::Base
     @account = recipient
     mail(:to => recipient[:email],
          :subject => "You just purchased an eCard from Artiphany, inside you will find your redeemable code",
-         :bcc => ["bcc@example.com", "Order Watcher <gabriel@simplecustomsolutions.com>"])
+         :bcc => ["ignacio@simplecustomsolutions.com", "Order Watcher <gabriel@simplecustomsolutions.com>"])
   end
   
   def recipient(content)
@@ -14,7 +14,7 @@ class CodeNotifier < ActionMailer::Base
     name = content[:sendername]
     messagesubject = name + " just sent you an eCard through Artiphany"
     mail(:to => content[:email],                                          
-         :bcc => [content[:senderemail], "Order Watcher <gabriel@simplecustomsolutions.com>"],
+         :bcc => ["ignacio@simplecustomsolutions.com", "Order Watcher <gabriel@simplecustomsolutions.com>"],
          :subject => messagesubject)  
   end
 end
