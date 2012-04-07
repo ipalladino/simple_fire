dc325c58f4501d147e1173707dc4fb0efd36c9b2
Ignacio Palladino <ignacio@flingo.tv>
2012-04-07 16:38:24 -0700

    Added attr_accessible pay_key to model

--- app/models/sent_ecard.rb
+++ app/models/sent_ecard.rb
@@ -1,6 +1,6 @@
 class SentEcard < ActiveRecord::Base
   belongs_to :ecard
   
-  attr_accessible :recipientemail , :recipientname, :message1, :message2, :nametoshow, :senderemail, :securelink, :ecard_id  , :sent
+  attr_accessible :recipientemail , :recipientname, :message1, :message2, :nametoshow, :senderemail, :securelink, :ecard_id  , :sent, :pay_key
   
 end
