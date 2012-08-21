742ff355514bb25762688a6dcfb23ff3b8085063
Dejan Cvijanovic <cdejan@gmail.com>
2012-08-21 15:12:30 +0200

    * Application settings points to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,26 +60,47 @@ D2mECards::Application.configure do
 
   # CUSTOM SETTINGS
 
+  #config.paypal_return_url = 'http://77.28.98.73:8202/transaction_complete'
+  #config.paypal_ipn_url = 'http://77.28.98.73:8202/transaction'
+  #config.paypal_cancel_url = 'http://77.28.98.73:8202/cancel_transaction'
+  #config.paypal_currentcy = 'USD'
+  #config.paypal_receiver_email = 'cdeyan_1338373228_biz@yahoo.com'
+  #
+  #config.ftp_server = 'localhost'
+  #config.ftp_user = 'rails'
+  #config.ftp_password = 'r@1L5l0l'
+  #config.ftp_swf_dir = '/artiphany_assets/ecard_swfs/'
+  #config.ftp_images_dir = '/artiphany_assets/ecard_images/'
+  #config.ftp_videos_dir = '/artiphany_assets/ecard_videos/'
+  #config.ftp_artiphany_assets_dir = '/artiphany_assets/'
+  #
+  #config.video_files_url = 'http://77.28.98.73:8202/animations/'
+  #
+  #config.email_from = 'webcomposer.info@gmail.com'
+  #config.email_return_path = 'system@example.com'
+  #config.email_bcc = ['cdejan@gmail.com', 'webcomposer.info@gmail.com']
+  #config.email_support = 'webcomposer.info@gmail.com'
+
+  # END OF CUSTOM SETTINGS
+
   config.paypal_return_url = 'http://77.28.98.73:8202/transaction_complete'
   config.paypal_ipn_url = 'http://77.28.98.73:8202/transaction'
   config.paypal_cancel_url = 'http://77.28.98.73:8202/cancel_transaction'
   config.paypal_currentcy = 'USD'
   config.paypal_receiver_email = 'cdeyan_1338373228_biz@yahoo.com'
 
-  config.ftp_server = 'localhost'
-  config.ftp_user = 'rails'
-  config.ftp_password = 'r@1L5l0l'
-  config.ftp_swf_dir = '/artiphany_assets/ecard_swfs/'
-  config.ftp_images_dir = '/artiphany_assets/ecard_images/'
-  config.ftp_videos_dir = '/artiphany_assets/ecard_videos/'
-  config.ftp_artiphany_assets_dir = '/artiphany_assets/'
+  config.ftp_server = 'ftp.simplecustomsolutions.com'
+  config.ftp_user = 'ipalladino'
+  config.ftp_password = 'ip8801ip'
+  config.ftp_swf_dir = '/simplecustomsolutions.com/artiphany_assets/ecard_swfs/'
+  config.ftp_images_dir = '/simplecustomsolutions.com/artiphany_assets/ecard_images/'
+  config.ftp_videos_dir = '/simplecustomsolutions.com/artiphany_assets/ecard_videos/'
+  config.ftp_artiphany_assets_dir = '/simplecustomsolutions.com/artiphany_assets/'
 
-  config.video_files_url = 'http://77.28.98.73:8202/animations/'
+  config.video_files_url = 'http://www.simplecustomsolutions.com/animations/'
 
-  config.email_from = 'webcomposer.info@gmail.com'
+  config.email_from = 'no-reply@artiphany.com'
   config.email_return_path = 'system@example.com'
   config.email_bcc = ['cdejan@gmail.com', 'webcomposer.info@gmail.com']
   config.email_support = 'webcomposer.info@gmail.com'
-
-  # END OF CUSTOM SETTINGS
 end
